17b110e3e3895ba35db41edab8a0948def69b6bd
Josue Valencia <josue.valencia@domo.ec>
2019-08-03 09:33:13 +0200

    wrapping the basic

--- app.js
+++ app.js
@@ -1,20 +1,59 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const mongoose = require('mongoose');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const createError = require('http-errors');
+const passport = require('passport');
+// configs
+require('./configs/db.config');
+require('./configs/passport.config');
+const session = require('./configs/session.config')
+const cors = require('./configs/cors.config')
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+// routers
+const authRouter = require('./routes/auth.routes');
 
-var app = express();
-
+// initializing express...
+const app = express();
+// middlewares
+// const secure = require('./middlewares/secure.mid');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+// authentication middlewares
+app.use(cors)
+app.use(session)
+app.use(passport.initialize())
+app.use(passport.session())
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/', authRouter);
+// app.use('/tests', secure.isAuthenticated, testRouter)
+
+
+//* Handling errors
+app.use((req, res, next) => {
+  next(createError(404))
+})
+
+app.use((error, req, res, next) => {
+  console.error(error);  
+  res.status(error.status || 500);
+  const data = {};
+
+  if (error instanceof mongoose.Error.ValidationError) {
+    res.status(400);
+    data.errors = {}
+    Object.keys(error.errors)
+      .forEach(field => data.errors[field] = error.errors[field].message)
+  } else if (error instanceof mongoose.Error.CastError) {
+    res.status(404);
+    error.message = 'Resource not found';
+  }
+
+  data.message = error.message
+  res.json(data);
+})
 
 module.exports = app;

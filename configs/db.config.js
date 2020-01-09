30333193592679146ccd4dea2c09dbbd2df0a8e9
josue valencia <josue.valrob@gmail.com>
2020-01-09 19:19:40 +0100

    (node:2811) DeprecationWarning: current Server Discovery and Monitoring engine is deprecated, and will be removed in a future version. To use the new Server Discover and Monitoring engine, pass option { useUnifiedTopology: true } to the MongoClient constructor.

--- configs/db.config.js
+++ configs/db.config.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const MONGODB_URI = process.env.MONGODB_URI;
 
-mongoose.connect(MONGODB_URI, { useCreateIndex: true, useNewUrlParser: true})
+mongoose.connect(MONGODB_URI, { useCreateIndex: true, useNewUrlParser: true,  useUnifiedTopology: true })
   .then(() => console.info(`Successfully connected to de database ${MONGODB_URI}`))
   .catch(error => console.error(`An error ocurred trying to connect to the database ${MONGODB_URI}`, error))

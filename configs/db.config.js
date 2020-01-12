6d90280bb97a1f147b192e75d98bcfc3ea0c3d51
josue valencia <josue.valrob@gmail.com>
2020-01-12 18:30:22 +0100

    adding credentials into the mongo db connection

--- configs/db.config.js
+++ configs/db.config.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const MONGODB_URI = process.env.MONGODB_URI;
-
-mongoose.connect(MONGODB_URI, { useCreateIndex: true, useNewUrlParser: true,  useUnifiedTopology: true })
+const uri = "mongodb+srv://"+ process.env.MONGO_USER +":"+ process.env.MONGO_PASS +"@" + process.env.MONGO_CLUSTER;
+mongoose.connect(uri, { useCreateIndex: true, useNewUrlParser: true,  useUnifiedTopology: true })
   .then(() => console.info(`Successfully connected to de database ${MONGODB_URI}`))
   .catch(error => console.error(`An error ocurred trying to connect to the database ${MONGODB_URI}`, error))

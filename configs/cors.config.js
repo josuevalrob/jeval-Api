1c4326a11d5dd7aa037961071a0419e197b96c27
josue valencia <josue.valrob@gmail.com>
2020-04-16 05:41:24 +0000

    allowed origings

--- configs/cors.config.js
+++ configs/cors.config.js
@@ -2,5 +2,6 @@ const cors = require('cors')
 
 module.exports = cors({
   credentials: true,
-  origin: process.env.ALLOW_ORIGINS
+  //origin: [process.env.ALLOW_ORIGINS]
+ origin:['https://josuevalrob.github.io/S2R2-PROJECT', 'https://josuevalrob.github.io']
 })

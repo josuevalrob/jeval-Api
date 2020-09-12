2a3fae9e65e92b20ae0c51fdfedb3d6375c86a3f
josue valencia <josue.valrob@gmail.com>
2020-09-12 15:46:11 +0200

    cleaning db.config

--- configs/db.config.js
+++ configs/db.config.js
@@ -1,6 +1,5 @@
 const mongoose = require('mongoose');
 const MONGODB_URI = process.env.MONGODB_URI;
-const uri = "mongodb+srv://"+ process.env.MONGO_USER +":"+ process.env.MONGO_PASS +"@" + process.env.MONGO_CLUSTER;
-mongoose.connect(uri, { useCreateIndex: true, useNewUrlParser: true,  useUnifiedTopology: true })
-  .then(() => console.info(`Successfully connected to de database ${process.env.MONGO_CLUSTER}`))
-  .catch(error => console.error(`An error ocurred trying to connect to the database ${process.env.MONGO_CLUSTER}`, error))
+mongoose.connect(MONGODB_URI, { useCreateIndex: true, useNewUrlParser: true,  useUnifiedTopology: true })
+  .then(() => console.info(`Successfully connected to de database ${process.env.MONGO_ENV}`))
+  .catch(error => console.error(`An error ocurred trying to connect to the database ${process.env.MONGO_ENV}`, error))
